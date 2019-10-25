1a7c6c285f1f3046a0c7e9710a74b5a0460735f7
Timur Chen <timur.guo.chen@gmail.com>
2019-10-25 17:30:11 +0800

    Meets Expectation

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { projects } = require('./data.json');
+const { projects } = require('../data.json');
 
 // setup main routes
 router.get('/', (req, res, next) => {
-    res.render('home', { projects });
+    res.render('index', { projects });
 });
 
 router.get('/about', (req, res, next) => {
     res.render('about');
-})
+});
 
 module.exports = router;
--- routes/project.js
+++ routes/project.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const { projects } = require('./data.json');
+const { projects } = require('../data.json');
 
 // setup project routes
+router.get('/', (req, res, next) => {
+    res.redirect(`/project/${Math.floor(Math.random() * projects.length)}`);
+});
+
 router.get('/:id', (req, res, next) => {
     const {id} = req.params;
-    res.render('project', {project: projects[id]});
+    let index = 0;
+    if (isNaN(id)) {
+        return res.redirect(`/project/${Math.floor(Math.random() * projects.length)}`);
+    } else {
+        index = Math.floor(id);
+        if (index < 0) {
+            index = 0;
+        } else if (index > (projects.length - 1)) {
+            index = projects.length - 1;
+        }
+    }
+    res.render('project', {project: projects[index]});
 });
+
+module.exports = router;
